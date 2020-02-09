355b6b74df541fbbc780591380df4dfb8652f9ac
Happy8-8 <gafonji@gmail.com>
2020-02-09 19:59:20 +0000

    Webpack issue resolves but others still pending

--- Gemfile
+++ Gemfile
@@ -55,6 +55,7 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'node'
 gem 'simple_form'
+gem 'devise'
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_09_061320) do
+ActiveRecord::Schema.define(version: 2020_02_09_193436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id"
+    t.integer "gram_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gram_id"], name: "index_comments_on_gram_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "grams", force: :cascade do |t|
     t.text "message"
@@ -21,6 +31,18 @@ ActiveRecord::Schema.define(version: 2020_02_09_061320) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
     t.index ["user_id"], name: "index_grams_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
